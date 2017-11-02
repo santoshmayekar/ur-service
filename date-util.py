8ca813cb10087bc74ddd4cab1fed39da7fa6ab7a
santosh <santosh@santosh-Vostro-15-3568.(none)>
2017-11-02 18:32:02 +0530

    second function month component

--- date-util.py
+++ date-util.py
@@ -1,3 +1,5 @@
 #this is a hypothetical code for date activities.
 
-#this has multiple codes recorded for date benefits
+#this has multiple codes recorded for date benefits.
+
+#a new function that returns month part of the date.
